1775feeb2871034341bb82d7339c898dec3005b4
kreed17 <kamron.reed98@gmail.com>
2022-01-24 17:56:29 -0600

    ok

--- src/components/About.js
+++ src/components/About.js
@@ -43,28 +43,3 @@ function About() {
 
 export default About
 
-
-//  <p className="mb-8 leading-relaxed">
-//             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-//             laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-//             Laborum, voluptas natus?
-//           </p>
-//           <div className="flex justify-center">
-//             <a
-//               href="#contact"
-//               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-//               Work With Me
-//             </a>
-//             <a
-//               href="#projects"
-//               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-//               See My Past Work
-//             </a>
-//           </div>
-//         </div>
-//         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-//           <img
-//             className="object-cover object-center rounded"
-//             alt="hero"
-//             src="./coding.svg"
-//           />
